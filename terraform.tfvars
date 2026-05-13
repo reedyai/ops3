b2563386346cae7b64a8a4a5beaa0ea1b4cf034f
Aiden <aolreedy57@gmail.com>
2026-05-12 20:32:26 -0700

    a

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
 ami_id        = "ami-091138d0f0d41ff90"
-instance_type = "t3.micro"
+instance_type = "t3.medium"
 key_name      = "cs312-key"
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.medium"
 }
 
 variable "key_name" {
